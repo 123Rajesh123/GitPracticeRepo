c05d5ec1e7d6c150dac2bff4c74595be04110b58
rajes <rajes@Rajeshpc>
2024-06-27 17:00:55 +0530

    new edit

--- src/test/java/ProductTest.java
+++ src/test/java/ProductTest.java
@@ -3,11 +3,11 @@ public class ProductTest {
 
 	public static void main(String[] args) {
 		System.out.println("Product is stable");
-<<<<<<< HEAD
+
 		System.out.println("product is stable but after unstable");
-=======
 		System.out.println("Product is not stable");
->>>>>>> branch 'master' of https://github.com/123Rajesh123/GitPracticeRepo.git
+		System.out.println("=========done=======");
+
 	}
 
 }
