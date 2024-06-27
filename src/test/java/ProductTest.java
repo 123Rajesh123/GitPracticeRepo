1218862f369eeee2ff45577ade3b48b669f1c106
rajes <rajes@Rajeshpc>
2024-06-27 16:32:31 +0530

    Add print statement in product by sam

--- src/test/java/ProductTest.java
+++ src/test/java/ProductTest.java
@@ -3,6 +3,7 @@ public class ProductTest {
 
 	public static void main(String[] args) {
 		System.out.println("Product is stable");
+		System.out.println("product is stable but after unstable");
 	}
 
 }
